bb707dbda2e7f059365baa4eca06d68bf0d9c9e8
bk <balukrish.2000@gmail.com>
2021-05-23 03:33:53 +0530

    AUfgabe 2.1

---
+++ Aufgabe2/hs_2.c
@@ -0,0 +1,11 @@
+/*Aufgabe hs_2
+ * Mister Muster - Matr.Nr.:575018330315 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\nName: Bala Krishnan Gopala Krishnan!\nMatrikel Nummer: 575018330315\n\n");
+	return 0;
+}
+
